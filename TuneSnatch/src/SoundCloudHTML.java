e110b7c9446bdf8f64f39e76feaca51088501137
nickdandakis <nickdandakis@gmail.com>
2013-07-21 13:31:16 +0100

    Fixed pull failing on SoundCloud HTML objects with pagenumber 0

--- TuneSnatch/src/SoundCloudHTML.java
+++ TuneSnatch/src/SoundCloudHTML.java
@@ -12,7 +12,7 @@ import com.gargoylesoftware.htmlunit.html.HtmlPage;
 public class SoundCloudHTML extends HTML {
 
 	private static final long serialVersionUID = 5589093030331078854L;
-	private static final boolean debug = true; // Toggles ability to save HTML document
+	private static final boolean debug = false; // Toggles ability to save HTML document
 
 	public SoundCloudHTML(String completeURL){
 		super(completeURL);
